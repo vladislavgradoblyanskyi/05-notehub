bc3e8756532804a2d952068cff30ea82da642978
VLADYSLAV HRADOBLIANSKYI <vladislavgradoblyanskyi@gmail.com>
2026-04-20 14:33:20 +0200

    Refactor App component to include Modal and NoteForm

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,19 @@
 import css from './App.module.css';
 import NoteList from '../NoteList/NoteList';
 import SearchBox from '../SearchBox/SearchBox';
-import Pagination from "../Pagination/Pagination.tsx";
+import Pagination from "../Pagination/Pagination";
+import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
+
 import { useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { useQuery } from "@tanstack/react-query";
 import { fetchNotes } from "../../services/noteService";
 
-
-
 export default function App() {
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleSearch = useDebouncedCallback((value: string) => {
     setSearch(value);
@@ -21,27 +23,35 @@ export default function App() {
   const { data, isLoading } = useQuery({
     queryKey: ["notes", search, page],
     queryFn: () => fetchNotes(page, search),
+    placeholderData: (prev) => prev,
   });
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
         <SearchBox onSearch={handleSearch} />
-        <button className={css.button}>Create note +</button>
+        <button className={css.button} onClick={() => setIsOpen(true)}>
+          Create note +
+        </button>
       </header>
 
       {isLoading && <p>Loading...</p>}
 
-      {data && data.notes && (
-        <NoteList notes={data.notes} />
-      )}
+      {data && data.notes && <NoteList notes={data.notes} />}
 
-      {data && data.totalPages > 1 && 
+      {data && data.totalPages > 1 && (
         <Pagination
           pageCount={data.totalPages}
+          currentPage={page}
           onPageChange={(newPage) => setPage(newPage)}
         />
-      }
+      )}
+
+      {isOpen && (
+        <Modal onClose={() => setIsOpen(false)}>
+          <NoteForm onClose={() => setIsOpen(false)} />
+        </Modal>
+      )}
     </div>
   );
 }
