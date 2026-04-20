b1fd1acd96594b1cd620f43186eff4ac3a4a27c5
VLADYSLAV HRADOBLIANSKYI <vladislavgradoblyanskyi@gmail.com>
2026-04-20 14:32:51 +0200

    Refactor Pagination component to use ReactPaginate

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,21 +1,31 @@
-import * as ReactPaginateImport from "react-paginate";
-import css from './Pagination.module.css'
-const Paginate =
-  (ReactPaginateImport as any).default?.default ||
-  (ReactPaginateImport as any).default ||
-  ReactPaginateImport;
-import type {PaginationProps} from '../../types/note'
-export default function Pagination({ pageCount, onPageChange }: PaginationProps) {
+import css from './Pagination.module.css';
+import type { ComponentType } from "react";
+import ReactPaginateModule from "react-paginate";
+import type { ReactPaginateProps } from "react-paginate";
+
+type ModuleWithDefault<T> = { default: T };
+
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<ComponentType<ReactPaginateProps>>
+).default;
+
+export interface PaginationProps {
+  pageCount: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export default function Pagination({pageCount,currentPage,onPageChange,}: PaginationProps) {
   return (
-    
-    <Paginate
+    <ReactPaginate
       pageCount={pageCount}
-      onPageChange={(evt: { selected: number }) => onPageChange(evt.selected + 1)}
       previousLabel="<"
       nextLabel=">"
       breakLabel="..."
       activeClassName={css.active}
       pageClassName={css.page}
+      forcePage={currentPage - 1}
+      onPageChange={(evt: { selected: number }) => onPageChange(evt.selected + 1)}
     />
   );
 }
